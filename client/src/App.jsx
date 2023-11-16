39920c63e4fe7ac82787ec041aebf8f1a55ef6a0
MHM056 <mihailmarholev@gmail.com>
2023-11-16 19:51:28 +0200

    Added game create page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Header } from "./components/header/Header"
 import { Home } from "./components/home/Home";
 import { GameList } from './components/game-list/GameList';
+import { GameCreate } from './components/game-create/GameCreate';
 
 
 function App() {
@@ -9,10 +10,11 @@ function App() {
   return (
     <div id="box">
       <Header />
-      
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/games' element={<GameList />} />
+        <Route path='/games/create' element={<GameCreate />} />
       </Routes>
     </div>
   )
