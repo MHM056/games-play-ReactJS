39e4014b3b347307c1b613d56d9dd9aa1a6f038a
MHM056 <mihailmarholev@gmail.com>
2023-11-16 19:17:27 +0200

    Added Header + Home components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
+import { Header } from "./components/header/Header"
+import { Home } from "./components/home/Home";
+
 function App() {
 
   return (
     <>
-
+    <div id="box">
+      <Header />
+      <Home />
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
