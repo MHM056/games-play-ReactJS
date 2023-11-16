0790186633609c7028c20c84d6e387d3abb0bcc5
MHM056 <mihailmarholev@gmail.com>
2023-11-16 19:46:40 +0200

    Added router library
    Added GameList component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
 import { Header } from "./components/header/Header"
 import { Home } from "./components/home/Home";
+import { GameList } from './components/game-list/GameList';
+
 
 function App() {
 
   return (
-    <>
     <div id="box">
       <Header />
-      <Home />
+      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/games' element={<GameList />} />
+      </Routes>
     </div>
-    </>
   )
 }
 
