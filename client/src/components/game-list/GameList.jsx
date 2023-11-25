5e6acde59d26579054e59d992df6299028552e97
MHM056 <mihailmarholev@gmail.com>
2023-11-25 20:06:07 +0200

    Moved games state from App to GameList component

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -1,15 +1,24 @@
+import { useEffect, useState } from "react";
 import { GameListItem } from "./game-list-item/GameListItem";
+import * as gameService from "../../services/gameService";
 
-export const GameList = ({
-    games
-}) => {
+export const GameList = () => {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => {
+                setGames(result);
+            });
+
+    }, []);
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-           {games.map(x => <GameListItem key={x._id} {...x}/>)}
-            
+            {games.map(x => <GameListItem key={x._id} {...x} />)}
+
             {games.length === 0 && (
-            <h3 className="no-articles">No articles yet</h3>
+                <h3 className="no-articles">No articles yet</h3>
             )}
         </section>
     );
