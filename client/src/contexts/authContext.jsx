25538c590f1f9da377d0338588e5b05666c61e59
MHM056 <mihailmarholev@gmail.com>
2023-11-27 23:23:43 +0200

    Create usePersistedState hook

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -1,7 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext } from "react";
 import { useNavigate } from 'react-router-dom';
 
 import * as authService from "./services/authService";
+import { usePersistedState } from "../hooks/usePersistedState";
 
 
 const AuthContext = createContext();
@@ -12,10 +13,7 @@ export const AuthProvider = ({
     children,
 }) => {
     const navigate = useNavigate();
-    const [auth, setAuth] = useState(() => {
-        localStorage.removeItem('accessToken');
-        return {};
-    });
+    const [auth, setAuth] = usePersistedState('auth', {});
 
     const loginSubmitHandler = async (values) => {
         const result = await authService.login(values.email, values.password);
