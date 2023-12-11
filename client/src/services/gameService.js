3298bcf993d06675723a770e3149959fd58b5112
MHM056 <mihailmarholev@gmail.com>
2023-12-11 18:30:49 +0200

    Update gameService.js
    Create getLatest games function

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -3,9 +3,20 @@ import * as request from './requester';
 const baseURL = 'http://localhost:3030/data/games';
 
 export const getAll = async () => {
-   const games = await request.get(baseURL);
-   return games;
+    const games = await request.get(baseURL);
+    return games;
 };
+
+export const getLatest = async () => {
+    const query = URLSearchParams({
+        offset: 0,
+        pageSize: 3
+    });
+
+    const latestGames = await request.get(`${baseURL}?sortBy=_createdOn%20desc${query}`);
+    
+    return latestGames;
+}
 
 export const getOne = async (gameId) => {
     const game = await request.get(`${baseURL}/${gameId}`);
